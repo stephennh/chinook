ea74363f7343be76f5b2352ea1bd14c08b0bc3c7
Stephen H <stephen210huang@hotmail.com>
2017-02-01 15:27:03 -0500

    3a/3b questions answered

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -17,12 +17,12 @@
     PSQL: SELECT SUM("tracks"."milliseconds") FROM "tracks"
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
-
-
+    ActiveQuery: Track.where(media_type_id: "MPEG audio file").order(unit_price: :desc)
+    PSQL: SELECT "tracks".* FROM "tracks" WHERE "tracks"."media_type_id" = $1 ORDER BY "tracks"."unit_price" DESC
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
-
-
+    ActiveQuery: Track.where(media_type_id: "MPEG audio file").maximum(:unit_price)
+    PSQL: SELECT MAX("tracks"."unit_price") FROM "tracks" WHERE "tracks"."media_type_id" = $1
 
 # 4) Find the 2 oldest artists.
 
